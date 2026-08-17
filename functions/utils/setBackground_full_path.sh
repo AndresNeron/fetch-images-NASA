8a13fff365e1d59ff41e810ed28c17e293623b86
AndresNeron <andres.finanzas0820@gmail.com>
2026-08-16 22:42:33 -0600

    turing update

--- functions/utils/setBackground_full_path.sh
+++ functions/utils/setBackground_full_path.sh
@@ -1,7 +1,30 @@
 #!/bin/bash
 
-# Receives absolute_path
+# Receives absolute_path as argument
 set_Background_full_path() {
-	absolute_path=$1
-	gsettings set org.gnome.desktop.background picture-uri-dark "file://$absolute_path"
+    local image="$1"
+
+    # Validate that an image path was provided
+    if [[ -z "$image" ]]; then
+        echo "[*] Error: No image path provided."
+        return 1
+    fi
+
+	echo "$image"
+
+	if echo "$DESKTOP_SESSION" | grep -qiE "xfce|xorg"; then
+        # Query all last-image property paths directly
+        for prop in $(xfconf-query -c xfce4-desktop -l | grep "last-image"); do
+            echo "$prop"
+            xfconf-query -c xfce4-desktop -p "$prop" -n -t string -s "$image"
+        done
+        xfdesktop --reload
+
+    elif echo "$DESKTOP_SESSION" | grep -qiE "gnome|ubuntu"; then
+        gsettings set org.gnome.desktop.background picture-uri "file:///$image"
+        gsettings set org.gnome.desktop.background picture-uri-dark "file:///$image"
+
+    else
+        echo "[*] Unsupported desktop environment: $DESKTOP_SESSION"
+    fi
 }
